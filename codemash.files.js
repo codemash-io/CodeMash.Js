e79ecf192ba57eef835746b112f0b8bb97fe2586
Ernestas Burokas <ernestas.burokas@presentconnection.nl>
2020-03-29 22:10:34 +0300

    Method for getting public file path + login url (including mode) in configuration

--- codemash.files.js
+++ codemash.files.js
@@ -4,62 +4,65 @@ import { CONFIG as Endpoints } from './routes';
 
 export async function downloadImage(fileId, optimization) {
 
-    let route = `${Config.apiUrl}${Endpoints.PROJECT.FILES.DOWNLOAD(fileId)}`;
+  let route = `${Config.apiUrl}${Endpoints.PROJECT.FILES.DOWNLOAD(fileId)}`;
 
-    if (optimization)
+  if (optimization) {
+    route = `${Config.apiUrl}${Endpoints.PROJECT.FILES.DOWNLOAD_OPTIMIZED(fileId, optimization)}`;
+  }
+  let response = await server.loadText(route,
     {
-        route = `${Config.apiUrl}${Endpoints.PROJECT.FILES.DOWNLOAD_OPTIMIZED(fileId, optimization)}`;
-    }
-    let response = await server.loadText(route,
-    {
-        method: 'GET',
-        headers: {
-            'X-CM-ProjectId': Config.projectId,
-            Authorization: `Bearer ${Config.secretKey}`,
-            Accept: 'text/plain',      
-        },
-        body: null,
+      method: 'GET',
+      headers: {
+        'X-CM-ProjectId': Config.projectId,
+        Authorization: `Bearer ${Config.secretKey}`,
+        Accept: 'text/plain',
+      },
+      body: null,
     });
-    
-    let result = {
-      fileId,
-      image : response
-    };
-    return result;
+
+  let result = {
+    fileId,
+    image: response
+  };
+  return result;
 }
 
 export async function uploadFile(fileUri, collectionName, recordId, uniqueFieldName) {
 
   const formData = new FormData();
-  
-  if (uniqueFieldName != null && uniqueFieldName != undefined){
+
+  if (uniqueFieldName != null && uniqueFieldName != undefined) {
     formData.append("uniqueFieldName", uniqueFieldName)
   }
-  
-  if (recordId != null && recordId != undefined){
+
+  if (recordId != null && recordId != undefined) {
     formData.append("recordId", recordId)
   }
-    
-  let filename = fileUri.substring(fileUri.lastIndexOf('/')+1);
+
+  let filename = fileUri.substring(fileUri.lastIndexOf('/') + 1);
   // let fileExtension = filename.substring(filename.lastIndexOf('.')+1);
 
   formData.append('file', {
-      uri: fileUri,
-      name: filename,
-      type: 'image/png'
+    uri: fileUri,
+    name: filename,
+    type: 'image/png'
   });
 
   let response = await fetch(`${Config.apiUrl}${Endpoints.PROJECT.DATABASE.COLLECTION.FILES.UPLOAD(collectionName)}`,
-  {
+    {
       method: 'POST',
       headers: {
-          'X-CM-ProjectId': Config.projectId,
-          Authorization: `Bearer ${Config.secretKey}`,
-          'Content-Type': 'multipart/form-data',   
-          'Accept': 'application/json',
+        'X-CM-ProjectId': Config.projectId,
+        Authorization: `Bearer ${Config.secretKey}`,
+        'Content-Type': 'multipart/form-data',
+        'Accept': 'application/json',
       },
       body: formData,
-  });  
+    });
 
   return response;
+}
+
+export function getFilePath(directory, fileName) {
+  return  `${Config.baseFilePath}/${directory}/${fileName}`;
 }
--- config.js
+++ config.js
@@ -1,15 +1,23 @@
 class Config {
-    constructor(){
+    constructor() {
         this.apiUrl = 'https://api.codemash.io';
     }
 
-    init(config){
+    init(config, env) {
         this.secretKey = config.secretKey;
         this.projectId = config.projectId;
         this.region = config.region;
         this.version = config.version;
         this.tablePageSize = config.tablePageSize || 10;
         this.maxFileSize = config.maxFileSize || 104857600;
+
+        let modeParameter = '';
+        if (env === 'development') {
+            modeParameter = '&mode=dev';
+        }
+
+        this.loginUrl = `${this.apiUrl}/auth/aad?projectId=${config.projectId}${modeParameter}`;
+        this.baseFilePath = `https://cm-${config.projectId}.s3.eu-central-1.amazonaws.com`;
     }
 }
 
