a2e7a66fe87dfec2a4c0692989aa6af43a6a1535
meow12 <herkus.dauksas@gmail.com>
2020-07-08 15:56:35 +0300

    updated payments

--- src/codemash.payments.js
+++ src/codemash.payments.js
@@ -1,14 +1,15 @@
 import * as server from '../server/server';
 import Config from '../config/config';
 import { CONFIG as Endpoints } from '../routes';
+import { objectOrStringToString, toQueryString } from '../utils/utils';
 
-export async function getOrder (id) {
+export async function getOrder ({ secretKey, id }) {
   const response = await server.loadJson(`${Config.apiUrl}${Endpoints.PROJECT.PAYMENTS.ORDERS.GET(id)}`,
     {
       method: 'GET',
       headers: {
         'X-CM-ProjectId': Config.projectId,
-        Authorization: `Bearer ${Config.secretKey}`,
+        Authorization: `Bearer ${secretKey || Config.secretKey}`,
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
@@ -17,55 +18,70 @@ export async function getOrder (id) {
   return response;
 }
 
-export async function getOrders (pageNumber, pageSize, sort, filter, userId) {
-  const response = await server.loadJson(`${Config.apiUrl}${Endpoints.PROJECT.PAYMENTS.ORDERS.GET_ALL}`,
+export async function getOrders ({ secretKey, pageNumber, pageSize, sort, filter, userId }) {
+  const request = {
+    userId,
+    pageSize: pageSize || Config.tablePageSize,
+    pageNumber: pageNumber || 0,
+    filter: objectOrStringToString(filter),
+    sort: objectOrStringToString(sort)
+  };
+
+  const requestUrl = `${Endpoints.PROJECT.PAYMENTS.ORDERS.GET_ALL}?${toQueryString(request)}`;
+
+  const response = await server.loadJson(`${Config.apiUrl}${requestUrl}`,
     {
       method: 'GET',
       headers: {
         'X-CM-ProjectId': Config.projectId,
-        Authorization: `Bearer ${Config.secretKey}`,
+        Authorization: `Bearer ${secretKey || Config.secretKey}`,
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        userId,
-        pageNumber,
-        pageSize,
-        sort,
-        filter
-      })
+      body: null
     });
   return response;
 }
 
-export async function createOrder (record) {
+export async function createOrder ({ secretKey, accountId, orderSchemaId, userId, lines, asGuest, customerDetails, isTest, meta }) {
   const response = await server.loadJson(`${Config.apiUrl}${Endpoints.PROJECT.PAYMENTS.ORDERS.CREATE}`,
     {
       method: 'POST',
       headers: {
         'X-CM-ProjectId': Config.projectId,
-        Authorization: `Bearer ${Config.secretKey}`,
+        Authorization: `Bearer ${secretKey || Config.secretKey}`,
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(record)
+      body: JSON.stringify({
+        accountId,
+        orderSchemaId,
+        userId,
+        lines,
+        asGuest,
+        customerDetails,
+        isTest,
+        meta
+      })
     });
 
-  return response.result;
+  return response;
 }
 
-export async function createPayseraTransaction (orderId, record) {
+export async function createPayseraTransaction ({ secretKey, orderId, mode }) {
   const response = await server.loadJson(`${Config.apiUrl}${Endpoints.PROJECT.PAYMENTS.TRANSACTIONS.CREATE_PAYSERA(orderId)}`,
     {
       method: 'POST',
       headers: {
         'X-CM-ProjectId': Config.projectId,
-        Authorization: `Bearer ${Config.secretKey}`,
+        Authorization: `Bearer ${secretKey || Config.secretKey}`,
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(record)
+      body: JSON.stringify({
+        mode
+      })
     });
 
-  return response.result;
+  return response;
 }
