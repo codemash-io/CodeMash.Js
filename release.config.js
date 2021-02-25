66ef1564b90f20537a547cf94afbb0b9af1bff06
Domantas from CodeMash <domantasjovaisas@gmail.com>
2021-02-26 00:07:43 +0200

    feat: semantic release has been added

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	branches: 'master',
+	repositoryUrl: 'https://github.com/codemash-io/CodeMash.Js',
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+		'@semantic-release/github',
+	],
+};
