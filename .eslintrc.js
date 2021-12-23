3dd08d06728aa478465384149726a23cbf931c6b
Tomas Vosylius <vsls.tomas@gmail.com>
2021-12-23 18:31:25 +0200

    chore: remove react related rule eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -81,7 +81,7 @@ module.exports = {
                 '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
               ],
               // Packages. `react` related packages come first.
-              ['^react', '^@?\\w'],
+              ['^@?\\w'],
               // Internal packages.
               [
                 '^(@|@store|@resources|@components|@constants|@helpers|@hooks|@lib|vendored-lib)(/.*|$)',
