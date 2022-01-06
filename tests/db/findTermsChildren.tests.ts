0b4c5f54b8c749b2e57236f1c41a3c61e4ab0f70
Tomas Vosylius <vsls.tomas@gmail.com>
2022-01-06 18:42:26 +0200

    tests(db): findTermsChildren

--- tests/db/findTermsChildren.tests.ts
+++ tests/db/findTermsChildren.tests.ts
@@ -3,6 +3,9 @@ import path from 'path';
 import { expect } from 'chai';
 import dotenv from 'dotenv';
 
+import { findTermsChildren } from '../../src/modules/database';
+import { FindTermsChildrenRequest } from '../../src/types/codemash.dtos';
+
 describe('findTermsChildren', () => {
   beforeEach(() => {
     dotenv.config({
@@ -10,5 +13,21 @@ describe('findTermsChildren', () => {
     });
   });
 
-  it('should', async () => {});
+  it('should return some taxonomy children for example project', async () => {
+    const request = new FindTermsChildrenRequest({
+      collectionName: 'countries',
+    });
+
+    const result = await findTermsChildren(request);
+    expect(result.response?.totalCount).to.be.greaterThanOrEqual(0);
+  });
+
+  it('should return an error for non-existent taxonomy', async () => {
+    const request = new FindTermsChildrenRequest({
+      collectionName: 'n0nExistentTaxonomy',
+    });
+
+    const response = await findTermsChildren(request);
+    expect(response.isError).to.be.true;
+  });
 });
