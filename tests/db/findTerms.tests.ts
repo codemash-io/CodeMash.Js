471c6b984c7eb6fd3fc770771083a7c241e7f275
Tomas Vosylius <vsls.tomas@gmail.com>
2022-01-06 18:37:48 +0200

    tests(db): findTerms

--- tests/db/findTerms.tests.ts
+++ tests/db/findTerms.tests.ts
@@ -3,6 +3,9 @@ import path from 'path';
 import { expect } from 'chai';
 import dotenv from 'dotenv';
 
+import { findTerms } from '../../src/modules/database';
+import { FindTermsRequest } from '../../src/types/codemash.dtos';
+
 describe('findTerms', () => {
   beforeEach(() => {
     dotenv.config({
@@ -10,5 +13,21 @@ describe('findTerms', () => {
     });
   });
 
-  it('should', async () => {});
+  it('should return some taxonomies for example project', async () => {
+    const request = new FindTermsRequest({
+      collectionName: 'countries',
+    });
+
+    const result = await findTerms(request);
+    expect(result.response?.result).length.to.be.greaterThanOrEqual(0);
+  });
+
+  it('should return an error for non-existent taxonomy', async () => {
+    const request = new FindTermsRequest({
+      collectionName: 'n0nExistentTaxonomy',
+    });
+
+    const response = await findTerms(request);
+    expect(response.isError).to.be.true;
+  });
 });
