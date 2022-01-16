b2a2c7e598c0d161a3bc577456751cbe71291341
Tomas Vosylius <vsls.tomas@gmail.com>
2022-01-16 13:04:30 +0200

    feat: cmConfig singleton

--- src/config/cmConfig.ts
+++ src/config/cmConfig.ts
@@ -16,13 +16,22 @@ export class CMConfig implements ICMConfig {
 
   public region?: string;
 
-  constructor() {
+  private static instance: CMConfig;
+
+  private constructor() {
     this.apiUrl = process.env.CODEMASH_API_URL || STATICS.CODEMASH_API_URL;
     this.apiKey = process.env.CODEMASH_API_KEY;
     this.projectId = process.env.CODEMASH_PROJECT_ID;
     this.cluster = process.env.CODEMASH_CLUSTER;
     this.baseFilePath = process.env.CODEMASH_BASE_FILE_PATH;
     this.region = process.env.CODEMASH_REGION;
+  }
+
+  public static getInstance() {
+    if (CMConfig.instance) return CMConfig.instance;
+
+    this.instance = new CMConfig();
+    return this.instance;
   }
 
   public isValid(): this is TValidCMClientConfig {
