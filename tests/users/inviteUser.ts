8df6d37d1387db58feb15da05f844eed14ac81b1
Tomas Vosylius <vsls.tomas@gmail.com>
2022-01-24 20:25:07 +0200

    test: simple user module tests

---
+++ tests/users/inviteUser.ts
@@ -0,0 +1,27 @@
+import path from 'path';
+
+import * as faker from '@faker-js/faker';
+import { expect } from 'chai';
+import dotenv from 'dotenv';
+
+import { inviteUser } from '../../src/modules/users';
+import { InviteUserRequest } from '../../src/types/codemash.dtos';
+
+describe('inviteUser', () => {
+  beforeEach(() => {
+    dotenv.config({
+      path: path.resolve(__dirname, '../data/config/.env'),
+    });
+  });
+
+  it('should create a new user invitation', async () => {
+    const request = new InviteUserRequest({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+    });
+
+    const result = await inviteUser(request);
+    expect(result).to.not.throw;
+  });
+});
