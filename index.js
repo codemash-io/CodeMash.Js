b1782fa9bee3b9753be3777adb05285337b8ca03
meow12 <herkus.dauksas@gmail.com>
2022-01-25 12:29:17 +0200

    fix(server events): export server events
    export server events

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import * as notificationsService from './src/components/push';
 import * as emailsService from './src/components/email';
 import * as codeService from './src/components/code';
 import * as paymentsService from './src/components/payments';
+import * as sseService from './src/components/sse';
 import configuration from './src/config';
 
 export const db = databaseService;
@@ -14,4 +15,5 @@ export const notifications = notificationsService;
 export const emails = emailsService;
 export const code = codeService;
 export const payments = paymentsService;
+export const sse = sseService;
 export const config = configuration;
